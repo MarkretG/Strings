74586b961258fc158b85faecad36eac62cdbbb76
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-07-01 19:50:18 +0530

    small change

--- LongestSubStringBetweenVowels.java
+++ LongestSubStringBetweenVowels.java
@@ -14,9 +14,9 @@ import java.util.Scanner;
 public class LongestSubStringBetweenVowels {
     public static void subString(String str)
     {
-        int k = 0,max=0,flag=0,m=0;
+        int k = 0,max=0,m=0;
         int[] a = new int[10];
-        String s,lon="";
+        String s;
         String[] sub=new String[10];
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
@@ -27,27 +27,25 @@ public class LongestSubStringBetweenVowels {
         for (int i =0; i < k; i++) {
             int len;
             if (i + 1 < k) {
-                s="";
                 s = str.substring(a[i]+1,a[i + 1]);
                 len = s.length();
-                if (len>max) {
-                    lon="";
-                    max = len;
-                    lon=s;
-                    flag=1;
+                if (len>=max) {
+                     max = len;
+                     sub[m]=s;
+                    m++;              
                 }
-               else if(len>=max)
-                {
-                    lon="";
-                    max=len;
-                    lon=s;
-                    flag=0;
-                    System.out.println(lon);
-                }
+               
             }
         }
-        if (flag==1)
-        System.out.println(lon);
+         int maxLength=sub[0].length();
+        for(int j=1;j<m;j++)
+            if(sub[j].length()>maxLength)
+                maxLength=sub[j].length();
+
+        for (int j=0;j<m;j++)
+            if(sub[j].length()==maxLength)
+                System.out.println(sub[j]);
+       
     }
     public static void main(String[] args) {
         String str;
